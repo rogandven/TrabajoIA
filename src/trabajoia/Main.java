243f6211b4265ce13eb929f4eb47c91c7b4c0bc5
Roger Venegas <roger.venegas2301@alumnos.ubiobio.cl>
2025-10-05 05:59:20 -0300

    Added user input

--- src/trabajoia/Main.java
+++ src/trabajoia/Main.java
@@ -18,56 +18,65 @@ public class Main {
      */
     public static void main(String[] args) {
         ArrayList<String> validRoutes;
-        MatrixTree matrixtree;
+        MatrixTree matrixtree = null;
         Scanner s = new Scanner(System.in);
-        int[][] initialState;
-        int[][] finalState;
+        int[][] initialState = null;
+        int[][] finalState = null;
         int size;
-        int option;
-        String bestRoute;
+        int option = -1;
+        String bestRoute = null;
+        boolean redoSetup = true;
         
         while (true) {
-            option = -1;
-            bestRoute = null;
-            validRoutes = new ArrayList<>();
-            matrixtree = null;
-            initialState = null;
-            finalState = null;
-            size = Input.getSizeFromUser(s);
-            if (Input.getYesOrNoQuestionFromUser(s, "¿El estado inicial va a ser aleatorio?")) {
-                initialState = Input.getMatrixFromArray(Input.getNumberArrayFromRandomNumbers(size), size);
-            } else {
-                initialState = Input.getMatrixFromUser(s, size, "estado_inicial");
+            if (redoSetup) {
+                size = Input.getSizeFromUser(s);
+                if (Input.getYesOrNoQuestionFromUser(s, "¿El estado inicial va a ser aleatorio?")) {
+                    initialState = Input.getMatrixFromArray(Input.getNumberArrayFromRandomNumbers(size), size);
+                } else {
+                    initialState = Input.getMatrixFromUser(s, size, "estado_inicial");
+                }
+                Board.printMatrix(initialState);
+
+                if (Input.getYesOrNoQuestionFromUser(s, "¿El estado final va a ser aleatorio?")) {
+                    finalState = Input.getMatrixFromArray(Input.randomizeExistingArray(Input.getArrayFromMatrix(initialState)), size);
+                } else {
+                    finalState = Input.getMatrixFromUser(s, size, "estado_final");
+                }
+                Board.printMatrix(finalState);
             }
 
-            if (Input.getYesOrNoQuestionFromUser(s, "¿El estado final va a ser aleatorio?")) {
-                finalState = Input.getMatrixFromArray(Input.randomizeExistingArray(Input.getArrayFromMatrix(initialState)), size);
-            } else {
-                finalState = Input.getMatrixFromUser(s, size, "estado_final");
-            }
+            validRoutes = new ArrayList<>();
 
             option = Input.getUserOption(s);
             
-            if (option == Constants.AMPLITUD) {
-                matrixtree = new Board(initialState, finalState);
-                ((Board)matrixtree).Amplitud(validRoutes);
-            } else if (option == Constants.PROFUNDIDAD) {
-                matrixtree = new Board(initialState, finalState);
-                ((Board)matrixtree).Profundidad(validRoutes);
-            } else if (option == Constants.BIDIRECCIONAL) {
-                matrixtree = new DoubleBoard(initialState, finalState);
-                ((DoubleBoard)matrixtree).Bidireccional(validRoutes);
-            }
-            
-            bestRoute = Board.getBestRoute(validRoutes);
-            if (bestRoute == null) {
-                System.out.println("No hay solución");
-            } else {
-                System.out.println("Mejor camino: " + bestRoute);
-            }
-            
-            if (Input.getYesOrNoQuestionFromUser(s, "¿Desea salir?")) {
-                return;
+            try {
+                if (option == Constants.AMPLITUD) {
+                    matrixtree = new Board(initialState, finalState);
+                    ((Board)matrixtree).Amplitud(validRoutes);
+                } else if (option == Constants.PROFUNDIDAD) {
+                    matrixtree = new Board(initialState, finalState);
+                    ((Board)matrixtree).Profundidad(validRoutes);
+                } else if (option == Constants.BIDIRECCIONAL) {
+                    matrixtree = new DoubleBoard(initialState, finalState);
+                    ((DoubleBoard)matrixtree).Bidireccional(validRoutes);
+                }
+
+                bestRoute = Board.getBestRoute(validRoutes);
+                if (bestRoute == null) {
+                    System.out.println("No hay solución");
+                } else {
+                    System.out.println("Mejor camino: " + bestRoute);
+                }
+
+                if (Input.getYesOrNoQuestionFromUser(s, "¿Desea salir?")) {
+                    return;
+                }
+                
+                redoSetup = !(Input.getYesOrNoQuestionFromUser(s, "¿Desea probar la misma matriz con otro metodo?"));
+            } catch (CustomException e) {
+                System.out.println("ERROR: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("ERROR: Error desconocido");
             }
         }
     }   
